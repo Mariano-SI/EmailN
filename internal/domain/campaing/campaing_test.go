e8fcb45836b272f722ce758bf654b9f55d54181b
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-10-18 12:39:08 -0300

    feat: teste unitario refatorado com testify

--- internal/domain/campaing/campaing_test.go
+++ internal/domain/campaing/campaing_test.go
@@ -2,22 +2,20 @@ package campaing
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewCampaing(t *testing.T) {
+	assert := assert.New(t)
 	name := "Campain X"
 	content := "Body"
 	contacts := []string{"email1@email.com", "email1@email.com"}
 
 	campaing := NewCampaing(name, content, contacts)
 
-	if campaing.ID != "1" {
-		t.Error("expected 1")
-	} else if campaing.Name != name {
-		t.Error("expected correct name")
-	} else if campaing.Content != content {
-		t.Error("expected correct content")
-	} else if len(campaing.Contacts) != len(contacts) {
-		t.Error("expected correct contacts")
-	}
+	assert.Equal(campaing.ID, "1")
+	assert.Equal(campaing.Name, name)
+	assert.Equal(campaing.Content, content)
+	assert.Equal(len(campaing.Contacts), len(contacts))
+	assert.Equal(campaing.ID, "1")
 }
