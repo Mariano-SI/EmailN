fee94c8a39ecb98f8efa5482f7d90e2041058b1b
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-10-19 21:39:00 -0300

    feat: primeiro endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"emailn/internal/contract"
+	"emailn/internal/domain/campaing"
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World"))
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	service := campaing.Service{}
+	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
+		var request contract.NewCampaing
+		err := render.DecodeJSON(r.Body, &request)
+		if err != nil {
+			println(err)
+		}
+		id, err := service.Create(request)
+
+		if err != nil {
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, map[string]string{"error": err.Error()})
+			return
+		}
+
+		render.Status(r, http.StatusCreated)
+		render.JSON(w, r, map[string]string{"id": id})
+
 	})
+
 	http.ListenAndServe(":3000", r)
 }
