9eb5d5c71a36c95d1a126ed750f5d35e6663c91b
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-10-22 21:47:28 -0300

    feat: agrupamento de rotas

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -19,17 +19,23 @@ func main() {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(endpoints.Auth)
-
+	
 	db := database.NewDb()
 	campaignRepository := database.CampaignRepository{Db: db}
 	campaignService := campaign.ServiceImp{Repository: &campaignRepository}
 	handler := endpoints.Handler{CampaignService: &campaignService}
+	
+	r.Route("/campaigns", func (r chi.Router)  {
+		r.Use(endpoints.Auth)
+		r.Get("/", endpoints.HandlerError(handler.CampaignGet))
+		r.Get("/{id}", endpoints.HandlerError(handler.CampaignGetById))
+		r.Delete("/{id}", endpoints.HandlerError(handler.CampaignDelete))
+		r.Post("/", endpoints.HandlerError(handler.CampaignPost))
+	})
 
-	r.Get("/campaigns", endpoints.HandlerError(handler.CampaignGet))
-	r.Get("/campaigns/{id}", endpoints.HandlerError(handler.CampaignGetById))
-	r.Delete("/campaigns/{id}", endpoints.HandlerError(handler.CampaignDelete))
-	r.Post("/campaigns", endpoints.HandlerError(handler.CampaignPost))
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
 
 	fmt.Println("🚀 Servidor rodando na porta 3000...")
 	log.Fatal(http.ListenAndServe(":3000", r))
