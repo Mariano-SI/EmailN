934248f0cf0c2455f8c642ebbcfb7a9a783122da
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-10-23 15:28:56 -0300

    refactor: refatorcao do metodo que envia emails de campanhas

--- internal/infrastructure/mail/send_mail.go
+++ internal/infrastructure/mail/send_mail.go
@@ -1,83 +1,27 @@
 package mail
 
 import (
+	"emailn/internal/domain/campaign"
 	"fmt"
 	"os"
 
 	"gopkg.in/gomail.v2"
 )
 
-func SendMail() error {
+func SendMail(campaing *campaign.Campaign) error {
 
 	dialer := gomail.NewDialer(os.Getenv("EMAIL_SMTP"), 587, os.Getenv("EMAIL_USER"), os.Getenv("GMAIL_APP_PASSWORD"))
+	var emails []string
+
+	for _, contact := range campaing.Contacts {
+		emails = append(emails, contact.Email)
+	}
 
 	message := gomail.NewMessage()
 	message.SetHeader("From", os.Getenv("EMAIL_USER"))
-	message.SetHeader("To", "marianocarlos42@gmail.com")
-	message.SetHeader("Subject", "Hello")
-	htmlBody := `
-	<!DOCTYPE html>
-	<html lang="pt-BR">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Email Teste</title>
-		<style>
-			body {
-				font-family: Arial, sans-serif;
-				background-color: #f9f9f9;
-				margin: 0;
-				padding: 20px;
-			}
-			.container {
-				max-width: 600px;
-				margin: 0 auto;
-				background: #ffffff;
-				border-radius: 8px;
-				padding: 20px;
-				box-shadow: 0 2px 6px rgba(0,0,0,0.1);
-			}
-			h1 {
-				color: #4F46E5;
-				text-align: center;
-			}
-			p {
-				color: #333333;
-				line-height: 1.6;
-			}
-			.footer {
-				text-align: center;
-				color: #888888;
-				font-size: 12px;
-				margin-top: 20px;
-			}
-			.button {
-				display: inline-block;
-				background-color: #4F46E5;
-				color: #ffffff;
-				padding: 10px 20px;
-				text-decoration: none;
-				border-radius: 5px;
-				font-weight: bold;
-				margin-top: 20px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<h1>Olá, Mariano 👋</h1>
-			<p>Este é um e-mail de teste enviado a partir da sua aplicação Go!</p>
-			<p>Se você está lendo isso formatado, significa que o envio HTML está funcionando corretamente 🎉</p>
-			<a href="https://golang.org" class="button">Visitar Golang</a>
-			<div class="footer">
-				<p>© 2025 EmailN System</p>
-			</div>
-		</div>
-	</body>
-	</html>
-	`
-
-	message.SetBody("text/html", htmlBody)
+	message.SetHeader("To", emails...)
+	message.SetHeader("Subject", campaing.Name)
+	message.SetBody("text/html", campaing.Content)
 
 	fmt.Println("Chegou aqui")
 	return dialer.DialAndSend(message)
