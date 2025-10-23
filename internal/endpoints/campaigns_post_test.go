06a42dbbd8c6fb6172adf3cbd41742eb73655822
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-10-23 08:38:09 -0300

    feat: adicionando createdBy aos testes do endpoint de post

--- internal/endpoints/campaigns_post_test.go
+++ internal/endpoints/campaigns_post_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"bytes"
+	"context"
 	"emailn/internal/contract"
 	"encoding/json"
 	"errors"
@@ -21,19 +22,28 @@ var (
 		Content: "Hi everyone",
 		Emails:  []string{"test@test.com"},
 	}
+	createdByExpected = "test@test.com"
 )
+
+func setup(body contract.NewCampaign, createdByExpected string) (*http.Request, *httptest.ResponseRecorder) {
+	var buf bytes.Buffer
+	json.NewEncoder(&buf).Encode(body)
+	req, _ := http.NewRequest("POST", "/", &buf)
+	ctx := context.WithValue(req.Context(), "email", createdByExpected)
+	req = req.WithContext(ctx)
+	res := httptest.NewRecorder()
+
+	return req, res
+}
 
 func Test_CampaignsPost_should_save_new_campaign(t *testing.T) {
 	assert := assert.New(t)
 	service := new(internalmock.CampaignServiceMock)
 	service.On("Create", mock.MatchedBy(func(request contract.NewCampaign) bool {
-		return request.Name == body.Name && request.Content == body.Content
+		return request.Name == body.Name && request.Content == body.Content && request.CreatedBy == createdByExpected
 	})).Return("12345", nil)
 	handler := Handler{service}
-	var buf bytes.Buffer
-	json.NewEncoder(&buf).Encode(body)
-	req, _ := http.NewRequest("POST", "/", &buf)
-	res := httptest.NewRecorder()
+	req, res := setup(body, createdByExpected)
 
 	_, status, err := handler.CampaignPost(res, req)
 
@@ -46,10 +56,7 @@ func Test_CampaignsPost_should_inform_error_when_exists(t *testing.T) {
 	service := new(internalmock.CampaignServiceMock)
 	service.On("Create", mock.Anything).Return("", errors.New("error"))
 	handler := Handler{service}
-	var buf bytes.Buffer
-	json.NewEncoder(&buf).Encode(body)
-	req, _ := http.NewRequest("POST", "/", &buf)
-	res := httptest.NewRecorder()
+	req, res := setup(body, createdByExpected)
 
 	_, _, err := handler.CampaignPost(res, req)
 
