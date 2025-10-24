2d3c652ade03eb37c213f5297f1a0ff0ab69b6e4
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-10-24 14:50:30 -0300

    feat: nova query que busca todas as campanhas com status stared que nao foram enviadas ainda

--- internal/infrastructure/database/campaign_repository.go
+++ internal/infrastructure/database/campaign_repository.go
@@ -37,3 +37,8 @@ func (cr *CampaignRepository) Delete(campaign *campaign.Campaign) error {
 	tx := cr.Db.Select("Contacts").Delete(campaign)
 	return tx.Error
 }
+func (cr *CampaignRepository) GetCampaignsToBeSent() ([]campaign.Campaign, error) {
+	var campaigns []campaign.Campaign
+	tx := cr.Db.Preload("Contacts").Find(&campaigns, "status = ? AND date_part('minute', now()::timestamp - updated_on::timestamp ) > ?", campaign.Started, 1)
+	return campaigns, tx.Error
+}
