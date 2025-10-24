831ac9de76e04d3d6b94945064708b66a44a28d5
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-10-24 14:21:56 -0300

    fix: novas constraints para banco de dados

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -23,13 +23,14 @@ type Contact struct {
 }
 
 type Campaign struct {
-	ID        string    `validate:"required" gorm:"size:50"`
-	Name      string    `validate:"min=5,max=24" gorm:"size:100"`
-	CreatedOn time.Time `validate:"required"`
-	Content   string    `validate:"min=5,max=1024" gorm:"size:1024"`
+	ID        string    `validate:"required" gorm:"size:50;not null"`
+	Name      string    `validate:"min=5,max=24" gorm:"size:100;not null"`
+	CreatedOn time.Time `validate:"required" gorm:"not null"`
+	UpdatedOn time.Time `validate:"required"`
+	Content   string    `validate:"min=5,max=1024" gorm:"size:1024;not null"`
 	Contacts  []Contact `validate:"min=1,dive"`
-	Status    string    `gorm:"size:20"`
-	CreatedBy string    `validate:"email" gorm:"size:100"`
+	Status    string    `gorm:"size:20;not null"`
+	CreatedBy string    `validate:"email" gorm:"size:100;not null"`
 }
 
 func (c *Campaign) Cancel() {
